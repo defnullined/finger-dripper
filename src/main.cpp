b36d3ca43927ca85933751ea4b6a89b4c5f6c63f
urandom <urandom43@gmail.com>
2020-04-15 23:59:46 +0400

    nothin

--- src/main.cpp
+++ src/main.cpp
@@ -13,9 +13,9 @@
 using namespace constants;
 
 int main() {
-   sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), TITLE);
-   window.setFramerateLimit(FPS); // TODO: poor old cpu support?
-   sf::Clock clock;
+    sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), TITLE);
+    window.setFramerateLimit(FPS); // TODO: poor old cpu support?
+    sf::Clock clock;
 
     // load font
     sf::Font font;

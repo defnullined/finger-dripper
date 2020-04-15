f15ecd5f9dc58fa923aab9185eb4e3184e650777
defnullined <62819817+defnullined@users.noreply.github.com>
2020-04-15 21:40:25 +0400

    Create constants.h

---
+++ include/constants.h
@@ -0,0 +1,19 @@
+/* Game constants */
+#ifndef CONSTANTS_H_INCLUDED
+#define CONSTANTS_H_INCLUDED
+
+#include <string>
+
+namespace constants {
+	const std::string TITLE = "Typing test";
+	const int FPS = 60;
+	const int WIDTH = 900;
+	const int HEIGHT = 700;
+
+	const int WORD_SIZE = 30;
+
+	const int STATUS_DISPLAY_HEIGHT = WIDTH/9;
+	const int STATUS_DISPLAY_WIDTH = WIDTH;
+}
+
+#endif
